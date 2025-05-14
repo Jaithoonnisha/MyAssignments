d8d0a40b86df84e7ee92972c5e6bfd64d0907802
Jaithoonnisha <jaithoon97@gmail.com>
2025-05-15 04:12:35 +0530

    Add files via upload
    Week 1 Day 1 commit

---
+++ week1/day1/CheckNumberIsPositive.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class CheckNumberIsPositive {
+//Control statements assignment
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		if(num>=0)
+		{
+			System.out.println("Number is postive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+	}
+	
+}
---
+++ week1/day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+//Datatype assignment
+	
+	public static void main(String[] args) {
+		
+		float browserVersion = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Chrome Browser"+"\n"+"Browser version :"+browserVersion+"\n"+"Developer :"+developer+"\n"+"Release year :"+releaseYear+"\n"+"Shortcut key :"+shortcutKey+"\n"+"Is a Beta version :"+isBeta);
+		
+	}
+}
